4a3952ebaa634cf26517e1f6844902b61d2e9569
Gabriele-V <gabriele@villanet.it>
2014-05-08 21:44:28 +0200

    New attachment

--- AttachmentList/luacontent.lua
+++ AttachmentList/luacontent.lua
@@ -1,16 +1,3 @@
-function get_base_path (BasePath,FilePath)
-    if BasePath == "DOCSDIR" then
-       BasePath = "#";
-    elseif BasePath == "MMEXDIR" then
-       BasePath = "#";
-    elseif BasePath == "DBDIR" then
-       BasePath = "#";
-    else
-       BasePath = "file:///" .. BasePath .. FilePath;
-   end
-    return BasePath;
-end
-
 function handle_record(record)
-    record:set("BasepathLUA", get_base_path(record:get("BasePath"),record:get("FilePath")));
+    record:set("BasepathLUA", record:GetDir(record:get("BasePath")) .. record:get("FilePath"));
 end
