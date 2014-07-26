adf49b378ffa6f207826afc95ce3e96be912d447
guanlisheng <guanlisheng@gmail.com>
2014-07-26 11:42:57 +0800

    scrip to pack general reports

---
+++ pack_gm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vi:tabstop=4:expandtab:shiftwidth=4:softtabstop=4:autoindent:smarttab
+
+import os
+import zipfile
+
+def pack_report(report):
+    print 'packing %s' % report
+    f = zipfile.ZipFile(report + '.zip', 'w')
+    for item in os.listdir(report):
+        f.write(os.path.join(report, item))
+    f.close()
+    print 'done %s' % report
+
+if __name__ == '__main__':
+    for report in os.listdir('.'):
+        if not report.startswith('.') and os.path.isdir(report):
+            try:
+                pack_report(report)
+            except:
+                exit(1)
+    exit(0)
+    
