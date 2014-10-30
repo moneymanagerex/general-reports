6f66048be986101c3a6311f82c9d771328ca5a7c
Lisheng Guan <guanlisheng@gmail.com>
2014-10-30 14:09:51 +0800

    refine stock summary

--- StockSummary/luacontent.lua
+++ StockSummary/luacontent.lua
@@ -1,15 +1,6 @@
 local total = 0;
 local count = 0;
 local colors = {"#FF6666", "#FFB266", "#FFFF66", "#B2FF66", "#66FF66", "#66FFB2", "#66FFFF", "#66B2FF", "#6666FF", "#B266FF", "#FF66FF", "#FF66B2"};
-local data = '';
-local json = [[
-    {value: %s,
-    color: "%s",
-    label: "%s",
-    labelColor: "black",
-    labelFontSize: "12",
-    labelAlign: "center"},
-    ]];
 
 function handle_record(record)
     local color = colors[1 + (count % #colors)];
@@ -17,10 +8,8 @@ function handle_record(record)
     record:set('COLOR', color);
     total = total + record:get('VALUE');
     local value = string.format("%.2f", record:get('VALUE'));
-    data = data .. string.format(json, value, color, record:get('SYMBOL'));
 end
 
 function complete(result)
     result:set('STOCK_TOTAL', total);
-    result:set('STOCK_DATA', data);
 end
