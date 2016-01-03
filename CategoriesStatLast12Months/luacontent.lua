858770348b798918890f796207c9bf5a4acad1ad
Arnaud Quirin <aquirin@gmail.com>
2016-01-04 00:28:09 +0100

    * Extension for incomes/expenses. * French translation.

--- CategoriesStatLast12Months/luacontent.lua
+++ CategoriesStatLast12Months/luacontent.lua
@@ -1,16 +1,32 @@
 local grandTotal = 0;
+local TotalWithdraw = 0;
+local TotalDeposit = 0;
 local cols = {["one"]=0, ["two"]=0, ["thr"]=0 , ["fou"]=0, ["fiv"]=0, ["six"]=0, ["sev"]=0, ["egh"]=0, ["nin"]=0, ["ten"]=0, ["ele"]=0, ["twe"]=0};
+local colsWithdraw = {["one"]=0, ["two"]=0, ["thr"]=0 , ["fou"]=0, ["fiv"]=0, ["six"]=0, ["sev"]=0, ["egh"]=0, ["nin"]=0, ["ten"]=0, ["ele"]=0, ["twe"]=0};
+local colsDeposit = {["one"]=0, ["two"]=0, ["thr"]=0 , ["fou"]=0, ["fiv"]=0, ["six"]=0, ["sev"]=0, ["egh"]=0, ["nin"]=0, ["ten"]=0, ["ele"]=0, ["twe"]=0};
 
 function handle_record(record)
     grandTotal = grandTotal + record:get('OVERALL');
     for key, value in pairs(cols) do
         cols[key] = cols[key] + record:get(key);
-    end
+        colsWithdraw[key] = colsWithdraw[key] + record:get("WITH_" .. key);
+        colsDeposit[key] = colsDeposit[key] + record:get("DEP_" .. key);
+        TotalWithdraw = TotalWithdraw + record:get("WITH_" .. key);
+        TotalDeposit = TotalDeposit + record:get("DEP_" .. key);
+     end
 end
 
 function complete(result)
     result:set("GRAND_TOTAL", grandTotal);
+    result:set("TOTAL_WITHDRAW", TotalWithdraw);
+    result:set("TOTAL_DEPOSIT", TotalDeposit);
     for key, value in pairs(cols) do
         result:set("TOTAL_" .. key, value);
+    end
+    for key, value in pairs(colsWithdraw) do
+        result:set("WITH_" .. key, value);
+    end
+    for key, value in pairs(colsDeposit) do
+        result:set("DEPOSIT_" .. key, value);
     end
 end
