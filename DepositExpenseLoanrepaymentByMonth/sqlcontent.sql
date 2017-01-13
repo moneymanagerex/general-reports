0012f250e435185136161e3cfb54e95b8bb119d5
Spoker <christophe.indevuyst@gmail.com>
2017-01-13 22:24:40 +0100

    Replace hardcoded accountID's by sql query

--- DepositExpenseLoanrepaymentByMonth/sqlcontent.sql
+++ DepositExpenseLoanrepaymentByMonth/sqlcontent.sql
@@ -1,21 +1,18 @@
-select 
+select
     'month' as periode_name,
     periode,
     sum(Deposit) as Deposit,
     sum(Withdrawal) as Withdrawal,
-	sum(Transfer) as Transfer,
+	  sum(Transfer) as Transfer,
     round(sum(Deposit) + sum(Withdrawal) + sum(Transfer),2) as Total,
     (
         (SELECT sum(initialbal) FROM accountlist_V1)
         +
-	(
-	    SELECT sum(totransamount) FROM checkingaccount_V1
-	    where
-                TRANSDATE <= date('now', 'start of month','-5 year','localtime')
-	)
+	      (SELECT sum(totransamount) FROM checkingaccount_V1
+	      where TRANSDATE <= date('now', 'start of month','-5 year','localtime'))
     ) as initialbal
-from (  
-    select 
+from (
+    select
         strftime('%Y-%m', TRANSDATE) as periode,
         case
             when transcode = 'Deposit' then transamount
@@ -26,10 +23,8 @@ from (
           else 0
         end as Withdrawal,
 		case
-			when transcode = 'Transfer' and toaccountid=('19') then -transamount
-			when transcode = 'Transfer' and toaccountid=('20') then -transamount
-			when transcode = 'Transfer' and accountid=('19') then transamount
-			when transcode = 'Transfer' and accountid=('20') then transamount	
+      when transcode = 'Transfer' and toaccountid in (select accountid from accountlist_V1 where accounttype='Loan') then -transamount
+      when transcode = 'Transfer' and accountid in (select accountid from accountlist_V1 where accounttype='Loan') then transamount
 			else 0
 		end as Transfer
         --,*
@@ -40,4 +35,4 @@ from (
         and status <>'V'
 )
 group by periode
-order by periode asc
+order by periode DESC
