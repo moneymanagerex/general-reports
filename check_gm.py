1f965618e30d1237f9ae42408715557460ae71af
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2018-12-26 14:46:33 +0100

    feature(check): validate report SQL against different DB schema versions

--- check_gm.py
+++ check_gm.py
@@ -1,38 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # vi:tabstop=4:expandtab:shiftwidth=4:softtabstop=4:autoindent:smarttab
 
-import os
+import os, sys
 import sqlite3
+import urllib.request
 
-def check(curs, report):
-    print 'checking %s' % report
-    sql = ''
-    for line in open(os.path.join(report, 'sqlcontent.sql'), 'rb'):
-        sql = sql + line
-    curs.execute(sql)
-    print 'done %s' % report
-
-if __name__ == '__main__':
-    conn = sqlite3.connect(':memory:')
-    conn.row_factory = sqlite3.Row 
-    curs = conn.cursor()
-    sql = ''
-    for line in open('tables_v1.sql', 'rb'):
-        sql = sql + line
-    curs.executescript(sql)
-    
-    anyNotPassed = False
-    
-    for report in os.listdir('.'):
-        if not report.startswith('.') and os.path.isdir(report):
-            try:
-                check(curs, report)
-            except:
-                print 'ERR: %s' % report
-    conn.close()
-    
-    if anyNotPassed:
-        exit(1)
-    
-    exit(0)
-    
+err = False
+for version in range (7, 14):
+    fname = 'tables_v1.sql' if version < 12 else 'tables.sql'
+    url = 'https://cdn.jsdelivr.net/gh/moneymanagerex/database@v%i/%s' % (version, fname)
+    schema = urllib.request.urlopen(url).read().decode('utf-8')
+    db = sqlite3.connect(':memory:')
+    db.executescript(schema)
+    print('\nTesting reports with MMEX db schema v%i:' % version)
+    print('-' * 40)
+    for root, dirs, files in os.walk('.'):
+        for sql in files:
+            if sql=='sqlcontent.sql':
+                try: db.executescript(open(os.path.join(root, sql)).read())
+                except sqlite3.Error as e:
+                    print('ERR', os.path.basename(root).ljust(40), e.args[0])
+                    err = True
+                else:
+                    print('OK ', os.path.basename(root))
+                db.rollback()
+    db.close()
+if err: sys.exit(1)
