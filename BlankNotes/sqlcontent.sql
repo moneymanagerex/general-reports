a78160ab28d54edd0cb7c8b4a230045f16cfc0c4
AO1 <44472390+andrewaimia@users.noreply.github.com>
2019-03-26 16:27:55 +0400

    Currency conversion and currency formatting.
    Pickup the latets currency conversion from the CURRENCYHISTORY_V1 table. If there are no value, then use CURRENCYFORMATS_V1.BASECONVRATE.

--- BlankNotes/sqlcontent.sql
+++ BlankNotes/sqlcontent.sql
@@ -1,11 +1,30 @@
-select 
-    t.TRANSID
-    ,TRANSDATE,ACCOUNTNAME,PAYEENAME
-    ,t.NOTES
-    , case t.TRANSCODE when 'Deposit' then TRANSAMOUNT else -TRANSAMOUNT end TRANSAMOUNT
-    ,m.PFX_SYMBOL, m.SFX_SYMBOL, m.GROUP_SEPARATOR, m.DECIMAL_POINT
-from CHECKINGACCOUNT_V1 as t
-	join PAYEE_V1 as p on p.PAYEEID=t.PAYEEID
-	join ACCOUNTLIST_V1 as a on a.ACCOUNTID=t.ACCOUNTID
-        join CURRENCYFORMATS_V1 as m on m.CURRENCYID=a.CURRENCYID
-where t.NOTES="";
+SELECT IFNULL(CH.CURRVALUE, m.BASECONVRATE) AS CURRRATE,
+    t.TRANSID,
+       t.TRANSDATE,
+       a.ACCOUNTNAME,
+       p.PAYEENAME,
+       t.NOTES,
+       ((CASE t.TRANSCODE WHEN 'Deposit' THEN t.TRANSAMOUNT ELSE -t.TRANSAMOUNT END) * (IFNULL(CH.CURRVALUE, m.BASECONVRATE))) AS TRANSAMOUNT,
+       m.PFX_SYMBOL,
+       m.SFX_SYMBOL,
+       m.GROUP_SEPARATOR,
+       m.DECIMAL_POINT
+  FROM CHECKINGACCOUNT_V1 AS t
+       JOIN
+       PAYEE_V1 AS p ON p.PAYEEID = t.PAYEEID
+       JOIN
+       ACCOUNTLIST_V1 AS a ON a.ACCOUNTID = t.ACCOUNTID
+       JOIN
+       CURRENCYFORMATS_V1 AS m ON m.CURRENCYID = a.CURRENCYID
+       LEFT JOIN
+       CURRENCYHISTORY_V1 AS CH ON CH.CURRENCYID = m.CURRENCYID AND 
+                                   CH.CURRDATE = (
+                                                     SELECT MAX(CRHST.CURRDATE) 
+                                                       FROM CURRENCYHISTORY_V1 AS CRHST
+                                                      WHERE CRHST.CURRENCYID = m.CURRENCYID
+                                                 )
+ WHERE t.NOTES;
+/* For v13 DB
+- remove the _V1 from the tablenames
+- remove the m.BASECONVRATE and ISNULL checks on CH.CURRVALUE
+*/
