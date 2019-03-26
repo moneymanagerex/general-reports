bad79a59604d3cc23cc7be9137ee8908a9bc0755
AO1 <44472390+andrewaimia@users.noreply.github.com>
2019-03-26 16:12:38 +0400

    Currency conversion and currency formatting.
    Pickup the latets currency conversion from the CURRENCYHISTORY_V1 table. If there are no value, then use CURRENCYFORMATS_V1.BASECONVRATE.

--- AccountSummary/sqlcontent.sql
+++ AccountSummary/sqlcontent.sql
@@ -1,17 +1,40 @@
-select a.ACCOUNTNAME, c.BASECONVRATE, c.PFX_SYMBOL, c.SFX_SYMBOL, c.DECIMAL_POINT, c.GROUP_SEPARATOR,
-    (select a.INITIALBAL + total(t.TRANSAMOUNT)
-    from
-        (select ACCOUNTID, STATUS,
-            (case when TRANSCODE = 'Deposit' then TRANSAMOUNT else -TRANSAMOUNT end) as TRANSAMOUNT
-        from CHECKINGACCOUNT_V1
-        union all
-        select TOACCOUNTID, STATUS, TOTRANSAMOUNT 
-        from CHECKINGACCOUNT_V1
-        where TRANSCODE = 'Transfer') as t
-    where  t.ACCOUNTID = a.ACCOUNTID
-        and t.STATUS NOT IN ('D', 'V')) as Balance
-from ACCOUNTLIST_V1 as a
-inner join CURRENCYFORMATS_V1 as c on c.CURRENCYID = a.CURRENCYID
-where a.ACCOUNTTYPE in ('Checking', 'Term') and a.STATUS = 'Open'
-group by a.ACCOUNTNAME
-order by a.ACCOUNTNAME asc;
+/* Setup: edit line 36 for the account types required */
+SELECT a.ACCOUNTNAME,
+       IFNULL(CH.CURRVALUE, c.BASECONVRATE) AS CURRVALUE, -- Remove this for v13 DB
+       c.PFX_SYMBOL,
+       c.SFX_SYMBOL,
+       c.DECIMAL_POINT,
+       c.GROUP_SEPARATOR,
+       (
+           SELECT a.INITIALBAL + total(t.TRANSAMOUNT) 
+             FROM (
+                      SELECT ACCOUNTID,
+                             STATUS,
+                             (CASE WHEN TRANSCODE = 'Deposit' THEN TRANSAMOUNT ELSE -TRANSAMOUNT END) AS TRANSAMOUNT
+                        FROM CHECKINGACCOUNT_V1
+                      UNION ALL
+                      SELECT TOACCOUNTID,
+                             STATUS,
+                             TOTRANSAMOUNT
+                        FROM CHECKINGACCOUNT_V1
+                       WHERE TRANSCODE = 'Transfer'
+                  )
+                  AS t
+            WHERE t.ACCOUNTID = a.ACCOUNTID AND 
+                  t.STATUS NOT IN ('D', 'V') 
+       )
+       AS BALANCE
+  FROM ACCOUNTLIST_V1 AS a
+       INNER JOIN
+       CURRENCYFORMATS_V1 AS c ON c.CURRENCYID = a.CURRENCYID
+       LEFT JOIN
+       CURRENCYHISTORY_V1 AS CH ON CH.CURRENCYID = c.CURRENCYID AND 
+                                   CH.CURRDATE = (
+                                                     SELECT MAX(CRHST.CURRDATE) 
+                                                       FROM CURRENCYHISTORY_V1 AS CRHST
+                                                      WHERE CRHST.CURRENCYID = c.CURRENCYID
+                                                 )
+ WHERE a.ACCOUNTTYPE IN ('Checking', 'Term') AND 
+       a.STATUS = 'Open'
+ GROUP BY a.ACCOUNTNAME
+ ORDER BY a.ACCOUNTNAME ASC;
