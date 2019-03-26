b17d8ba93dd12bfcb3004caad0d147d5df1bbf05
AO1 <44472390+andrewaimia@users.noreply.github.com>
2019-03-26 16:59:53 +0400

    SQL: Get Latest Curr Rate & Categories without SubCategories
    Pickup the latets currency conversion from the CURRENCYHISTORY_V1 table. If there are no value, then use CURRENCYFORMATS_V1.BASECONVRATE.
    Changed the SUBCATEGORY_V1 to left join as not all Categories have a subcategory (Other Expenses) and Transactions can be put at Category level by user.

--- CategoryTrend/sqlcontent.sql
+++ CategoryTrend/sqlcontent.sql
@@ -1,19 +1,37 @@
--- TODO: Update category and subcategory name in line 17. 
-select strftime('%Y', TRANSDATE) as YEAR
-    , strftime('%m', TRANSDATE) as MONTH
-    , total((case when TRANSCODE = 'Deposit' then TRANSAMOUNT else -TRANSAMOUNT end) * c1.BASECONVRATE) as AMOUNT
-from
-    (select ACCOUNTID, TRANSDATE, TRANSCODE, CATEGID, SUBCATEGID, TRANSAMOUNT from CHECKINGACCOUNT_V1
-        where STATUS NOT IN ('D', 'V')
-    union all
-    select t1.ACCOUNTID, t1.TRANSDATE, t1.TRANSCODE, t2.CATEGID, t2.SUBCATEGID, t2.SPLITTRANSAMOUNT from SPLITTRANSACTIONS_V1 as t2
-        inner join CHECKINGACCOUNT_V1 as t1
-        on t1.TRANSID = t2.TRANSID
-        where t1.STATUS NOT IN ('D', 'V')) as t3
-inner join ACCOUNTLIST_V1 as a1 on a1.ACCOUNTID = t3.ACCOUNTID
-inner join CURRENCYFORMATS_V1 as c1 on c1.CURRENCYID = a1.CURRENCYID
-inner join CATEGORY_V1 as cat on t3.CATEGID = cat.CATEGID
-inner join SUBCATEGORY_V1 as subcat on t3.SUBCATEGID = subcat.SUBCATEGID
-where cat.CATEGNAME = 'Food' and subcat.SUBCATEGNAME = 'Groceries'
-group by year, month
-order by year asc, month asc;
+-- TODO: Update category and subcategory name in line 35. 
+SELECT strftime('%Y', TRANSDATE) AS YEAR,
+       strftime('%m', TRANSDATE) AS MONTH,
+       total( (CASE WHEN TRANSCODE = 'Deposit' THEN TRANSAMOUNT ELSE -TRANSAMOUNT END) * IFNULL(CH.CURRVALUE, C1.BASECONVRATE) ) AS AMOUNT
+  FROM (
+           SELECT ACCOUNTID,
+                  TRANSDATE,
+                  TRANSCODE,
+                  CATEGID,
+                  SUBCATEGID,
+                  TRANSAMOUNT
+             FROM CHECKINGACCOUNT_V1
+            WHERE STATUS NOT IN ('D', 'V') 
+           UNION ALL
+           SELECT t1.ACCOUNTID,
+                  t1.TRANSDATE,
+                  t1.TRANSCODE,
+                  t2.CATEGID,
+                  t2.SUBCATEGID,
+                  t2.SPLITTRANSAMOUNT
+             FROM SPLITTRANSACTIONS_V1 AS t2
+                  INNER JOIN
+                  CHECKINGACCOUNT_V1 AS t1 ON t1.TRANSID = t2.TRANSID
+            WHERE t1.STATUS NOT IN ('D', 'V') 
+       )
+       AS t3
+       INNER JOIN ACCOUNTLIST_V1 AS a1 ON a1.ACCOUNTID = t3.ACCOUNTID
+       INNER JOIN CURRENCYFORMATS_V1 AS c1 ON c1.CURRENCYID = a1.CURRENCYID
+       INNER JOIN CATEGORY_V1 AS cat ON t3.CATEGID = cat.CATEGID
+       LEFT JOIN SUBCATEGORY_V1 AS subcat ON t3.SUBCATEGID = subcat.SUBCATEGID
+       LEFT JOIN CURRENCYHISTORY_V1 AS CH ON CH.CURRENCYID = C1.CURRENCYID AND 
+                                   CH.CURRDATE = ( SELECT MAX(CRHST.CURRDATE) 
+                                                       FROM CURRENCYHISTORY_V1 AS CRHST
+                                                      WHERE CRHST.CURRENCYID = C1.CURRENCYID)
+ WHERE cat.CATEGNAME = 'Food' AND subcat.SUBCATEGNAME = 'Groceries'
+ GROUP BY year, month
+ ORDER BY year ASC, month ASC;
