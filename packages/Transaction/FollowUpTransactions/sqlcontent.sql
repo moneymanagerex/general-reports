576cbf35ca28d34b5934c5b38bcad9ded6ca10e1
vomikan <nikolay.akimov@gmail.com>
2022-05-26 12:08:45 +0300

    fix(trx): update

--- packages/Transaction/FollowUpTransactions/sqlcontent.sql
+++ packages/Transaction/FollowUpTransactions/sqlcontent.sql
@@ -1,18 +1,26 @@
-select t.PAYEENAME, t.TRANSDATE, t.TRANSAMOUNT
-    , t.TRANSAMOUNT*c.BASECONVRATE as BaseAmount
-    , c.PFX_SYMBOL, c.SFX_SYMBOL
-from
-    (select p.PAYEENAME as PAYEENAME, c1.TRANSDATE as TRANSDATE, c1.STATUS as STATUS,
-        (case when c1.TRANSCODE = 'Deposit' then c1.TRANSAMOUNT else -c1.TRANSAMOUNT end) as TRANSAMOUNT,
-        a1.CURRENCYID as CURRENCYID
-    from CHECKINGACCOUNT_V1 as c1
-    inner join PAYEE_V1 as p on p.PAYEEID = c1.PAYEEID
-    inner join ACCOUNTLIST_V1 as a1 on a1.ACCOUNTID = c1.ACCOUNTID
-    union all
-    select a2.ACCOUNTNAME, c2.TRANSDATE, c2.STATUS, c2.TOTRANSAMOUNT, a2.CURRENCYID
-    from CHECKINGACCOUNT_V1 as c2
-    inner join ACCOUNTLIST_V1 as a2 on a2.ACCOUNTID = c2.TOACCOUNTID
-    where TRANSCODE = 'Transfer') as t
-inner join CURRENCYFORMATS_V1 as c on t.CURRENCYID=c.CURRENCYID
-where t.STATUS = 'F'
-order by t.PAYEENAME asc, t.TRANSDATE asc;
+SELECT IFNULL(CH.CURRVALUE, m.BASECONVRATE) AS CURRRATE,
+    t.TRANSID,
+       t.TRANSDATE,
+       a.ACCOUNTNAME,
+       p.PAYEENAME,
+       t.NOTES,
+       ((CASE t.TRANSCODE WHEN 'Deposit' THEN t.TRANSAMOUNT ELSE -t.TRANSAMOUNT END) * (IFNULL(CH.CURRVALUE, m.BASECONVRATE))) AS TRANSAMOUNT,
+       m.PFX_SYMBOL,
+       m.SFX_SYMBOL,
+       m.GROUP_SEPARATOR,
+       m.DECIMAL_POINT
+  FROM CHECKINGACCOUNT_V1 AS t
+       JOIN
+       PAYEE_V1 AS p ON p.PAYEEID = t.PAYEEID
+       JOIN
+       ACCOUNTLIST_V1 AS a ON a.ACCOUNTID = t.ACCOUNTID
+       JOIN
+       CURRENCYFORMATS_V1 AS m ON m.CURRENCYID = a.CURRENCYID
+       LEFT JOIN
+       CURRENCYHISTORY_V1 AS CH ON CH.CURRENCYID = m.CURRENCYID AND 
+                                   CH.CURRDATE = (
+                                                     SELECT MAX(CRHST.CURRDATE) 
+                                                       FROM CURRENCYHISTORY_V1 AS CRHST
+                                                      WHERE CRHST.CURRENCYID = m.CURRENCYID
+                                                 )
+ WHERE t.STATUS="F";
